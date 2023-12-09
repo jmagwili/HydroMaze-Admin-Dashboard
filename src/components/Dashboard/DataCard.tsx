f50f92e5cec6f5b263d29deb6a60d862c2ac3060
jmagwili <joshuamagwili@gmail.com>
2023-12-10 07:30:02 +0800

    fixed card icons at dashboard

--- src/components/Dashboard/DataCard.tsx
+++ src/components/Dashboard/DataCard.tsx
@@ -1,16 +1,22 @@
 import '../../styles/DataCard.css'
+import {ReactElement} from 'react'
+
 type DataCardProps = {
     title: string
     content: number
     color: string
+    icon: ReactElement
 }
 
-export const DataCard: React.FC<DataCardProps> = ({title,content,color}) => {
+export const DataCard: React.FC<DataCardProps> = ({title,content,color,icon}) => {
     return(
         <div className="datacard" style={{backgroundColor:`${color}`}}>          
             <h3 className="datacard-title">{title}</h3>
             <div className="datacard-content">
                 <p>{content}</p>
+            </div>
+            <div className="datacard-icon">
+                {icon}
             </div>
         </div>
     )
