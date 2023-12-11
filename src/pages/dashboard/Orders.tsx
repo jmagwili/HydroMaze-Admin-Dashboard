3bc2c3c15db25ab4be733e17a919e9769fafed26
shannonjohn <shanjohncantz@gmail.com>
2023-12-11 23:46:08 +0800

    fixed minor bug

--- src/pages/dashboard/Orders.tsx
+++ src/pages/dashboard/Orders.tsx
@@ -21,17 +21,20 @@ const Orders = () => {
           "http://localhost:4001/api/v1/orders/"
         );
         setOrders(userData.data);
+        console.log(userData.data);
       } catch (e) {
         console.log("Failed to fetch data\n", e);
       }
     };
     fetchData();
-  });
+  },[]);
 
   return (
     <div>
       <h1 className="ml-5 mt-5 font-semibold text-gray-800 text-3xl">ORDERS</h1>
       <hr className="m-2" />
+
+
     </div>
   );
 };
