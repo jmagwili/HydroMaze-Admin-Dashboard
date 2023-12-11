e0928fbe36c2156260e7a1de8360dac58960a05e
shannonjohn <shanjohncantz@gmail.com>
2023-12-11 23:58:02 +0800

    created orders table

--- src/pages/dashboard/Orders.tsx
+++ src/pages/dashboard/Orders.tsx
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
-
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+interface Orders {
+  _id: string;
+  username: string;
+  round: number;
+  slim: number;
+  total: number;
+  isOwned: boolean;
+  status: string;
+  createdAt: Date;
+}
 const Orders = () => {
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState<Orders[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,7 +43,35 @@ const Orders = () => {
     <div>
       <h1 className="ml-5 mt-5 font-semibold text-gray-800 text-3xl">ORDERS</h1>
       <hr className="m-2" />
+      <div>
+          <Table>
+              <TableCaption>
+                All of your orders
+              </TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Username</TableHead>
+                  <TableHead>Round Orders</TableHead>
+                  <TableHead>Slim Orders</TableHead>
+                  <TableHead>Total</TableHead>
+                  <TableHead>Status</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {orders.map((order) => (
+                  <TableRow key={order._id}>
+                    <TableCell>{order.username}</TableCell>
+                    <TableCell>{order.round}</TableCell>
+                    <TableCell>{order.slim}</TableCell>
+                    <TableCell>{order.total}</TableCell>
+                    <TableCell>{order.status}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+          </Table>
 
+
+      </div>
 
     </div>
   );
