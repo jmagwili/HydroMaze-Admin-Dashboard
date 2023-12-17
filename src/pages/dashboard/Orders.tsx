09eb7caef8e63287460d9cbde970c4e206c02e35
jmagwili <joshuamagwili@gmail.com>
2023-12-17 16:37:46 +0800

    added search card to orders page

--- src/pages/dashboard/Orders.tsx
+++ src/pages/dashboard/Orders.tsx
@@ -9,7 +9,38 @@ import {
   TableBody,
   TableCell,
 } from "@/components/ui/table";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { Badge } from "@/components/ui/badge";
+import { addDays, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { DateRange } from "react-day-picker"
+ 
+import { cn } from "@/lib/utils"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+
 interface Orders {
   _id: string;
   username: string;
@@ -22,9 +53,18 @@ interface Orders {
   date: string;
   time: string;
 }
+
+interface orderProp {
+  className: string;
+}
+
 import SidebarContext from "@/SidebarContext";
 
-const Orders = () => {
+const Orders:React.FC<orderProp> = ({className}) => {
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  })
   const [orders, setOrders] = useState<Orders[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [ordersPerPage] = useState(10);
@@ -66,6 +106,10 @@ const Orders = () => {
     
   },[expanded])
 
+  const searchStyle:CSSProperties = {
+    //border:"solid"
+  }
+
 
   const indexOfLastOrder = currentPage * ordersPerPage;
   const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
@@ -78,6 +122,77 @@ const Orders = () => {
       <h1 className="ml-5 mt-5 font-semibold text-gray-800 text-3xl">ORDERS</h1>
       <hr className="mt-2 mb-20" />
       <div className="ml-20 ">
+      <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Search Order</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your customer" />
+            </div>
+            <div className={cn("grid gap-2", className)}>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    id="date"
+                    variant={"outline"}
+                    className={cn(
+                      "w-[300px] justify-start text-left font-normal",
+                      !date && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date?.from ? (
+                      date.to ? (
+                        <>
+                          {format(date.from, "LLL dd, y")} -{" "}
+                          {format(date.to, "LLL dd, y")}
+                        </>
+                      ) : (
+                        format(date.from, "LLL dd, y")
+                      )
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    initialFocus
+                    mode="range"
+                    defaultMonth={date?.from}
+                    selected={date}
+                    onSelect={setDate}
+                    numberOfMonths={2}
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Order Status</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">confirmed</SelectItem>
+                  <SelectItem value="sveltekit">pending</SelectItem>
+                  <SelectItem value="astro">rejected</SelectItem>
+                  <SelectItem value="nuxt">delivered</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button>Search</Button>
+      </CardFooter>
+    </Card>
         <Table className="text-lg">
           <TableCaption>All of your orders</TableCaption>
           <TableHeader>
