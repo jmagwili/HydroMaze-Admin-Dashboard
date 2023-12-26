fafcb3fc1c6c60ff3272e04cf6ee17af53c12251
jmagwili <joshuamagwili@gmail.com>
2023-12-26 12:57:02 +0800

    fixed bug with search order controller

--- server/controllers/orders.controller.js
+++ server/controllers/orders.controller.js
@@ -13,17 +13,21 @@ const getAllOrders = async (req, res) => {
 };
 
 const searchOrder = async (req, res) => {
-  try{
-    const orders = await Orders.find({
-      username: req.body.name,
-      status: req.body.status
-    })
-    console.log(req.body)
-    res.json(orders)
-  }catch (error) {
+  try {
+    let query = {};
+    if (req.body.name) query.username = await req.body.name;
+    if (req.body.status) query.status = await req.body.status;
+
+    const orders = await Orders.find(query);
+
+    console.log("Successfullt fetched searched orders");
+
+    res.json(orders);
+  } catch (error) {
+    console.error("Error:", error);
     res.status(500).json({ message: error.message });
-    console.log(error);
   }
-}
+};
+
 
 export { getAllOrders, searchOrder };
