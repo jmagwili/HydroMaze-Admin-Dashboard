054393dc91b95504d97d5b27c8f29c3b84ba88c4
jmagwili <joshuamagwili@gmail.com>
2023-12-26 12:57:37 +0800

    fixed search order request

--- server/routes/orders.router.js
+++ server/routes/orders.router.js
@@ -6,6 +6,5 @@ const router = express.Router();
 
 //http://127.0.0.1:5173//api/v1/orders
 router.route('/').get(getAllOrders);
-router.route('/search').get(searchOrder);
-
+router.route('/search').post(searchOrder)
 export default router;
