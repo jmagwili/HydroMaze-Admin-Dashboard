ce8a12f992e94c6e3a356166a2b9631c5b8d4b46
jmagwili <joshuamagwili@gmail.com>
2023-12-26 13:34:32 +0800

    added date range search to search order controller

--- server/controllers/orders.controller.js
+++ server/controllers/orders.controller.js
@@ -14,20 +14,30 @@ const getAllOrders = async (req, res) => {
 
 const searchOrder = async (req, res) => {
   try {
-    let query = {};
+    const startDate = new Date(req.body.startDate);
+    const endDate = new Date(req.body.endDate);
+
+    endDate.setDate(endDate.getDate() + 1);
+
+    let query = {
+      createdAt: { $gte: startDate, $lt: endDate},
+    };
+
     if (req.body.name) query.username = await req.body.name;
-    if (req.body.status) query.status = await req.body.status;
+    if (req.body.status !== "null") query.status = await req.body.status;
+
+    console.log(query);
 
     const orders = await Orders.find(query);
 
-    console.log("Successfullt fetched searched orders");
-
+    console.log("Successfully fetched searched orders");
     res.json(orders);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ message: error.message });
   }
 };
+
 
 
 export { getAllOrders, searchOrder };
