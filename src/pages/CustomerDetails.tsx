e0ccc103255f393818d9a420e7a663cb72b4fb55
jmagwili <joshuamagwili@gmail.com>
2023-12-27 22:55:41 +0800

    integrated mapbox to customer details page

--- src/pages/CustomerDetails.tsx
+++ src/pages/CustomerDetails.tsx
@@ -1,21 +1,48 @@
 import SidebarContext from "@/SidebarContext"
 import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import ReactMapGL, { Marker } from "react-map-gl";
 import { Button } from "@/components/ui/button"
 import axios from "axios"
+import { FaLocationDot } from "react-icons/fa6";
 import '../styles/CustomerDetails.css'
+import "mapbox-gl/dist/mapbox-gl.css";
 
 interface UserData {
     phone: string;
     picture: string;
     username: string;
-    location: [{
-        locName: string;
-        address: string;
-        latitude: number;
-        longitude: number;
-    }]
-}
+    location: {
+      locName: string;
+      address: string;
+      latitude: number;
+      longitude: number;
+    }[];
+  }
+  
+  interface LocData {
+    data: [
+      {
+        phone: string;
+        picture: string;
+        username: string;
+        location: [
+          {
+            locName: string;
+            address: string;
+            latitude: number;
+            longitude: number;
+          }
+        ];
+      }
+    ];
+  }
+  
+  interface Viewport {
+    longitude: number;
+    latitude: number;
+    zoom: number;
+  }
 
 export default function CustomerDetails() {
     const { expanded } = useContext(SidebarContext)
@@ -23,13 +50,25 @@ export default function CustomerDetails() {
     const [expandedClass, setExpandedClass] = useState("")
     const [userDetails, setUserDetails] = useState<UserData>()
     const [isLoading, setIsLoading] = useState(true)
+    const [viewports, setViewports] = useState<Viewport[] | undefined>();
 
     useEffect(()=>{
         const fetchData = async () => {
             try{
-                const userData = await axios.get(`http://localhost:4001/api/v1/customers/${id}`)
+                const userData:LocData = await axios.get(`http://localhost:4001/api/v1/customers/${id}`)
                 setUserDetails(userData.data[0])
-                console.log(userData.data[0])
+                
+                let structuredLocData:Viewport[] = []
+
+                for(let i in userData.data[0].location){
+                    structuredLocData.push({
+                        longitude: userData.data[0].location[i].longitude,
+                        latitude: userData.data[0].location[i].latitude,
+                        zoom: 15,
+                    })
+                }
+
+                setViewports(structuredLocData)
                 setIsLoading(false)
             }catch(err){
                 console.log(err)
@@ -42,6 +81,11 @@ export default function CustomerDetails() {
         setExpandedClass(!expanded ? "notExpanded" : "")
     },[expanded])
 
+    useEffect(()=>{
+        console.log(userDetails)
+    },[userDetails])
+
+
     return(
         <div className={`customer-details ${expandedClass}`}>
             <h1 
@@ -50,25 +94,85 @@ export default function CustomerDetails() {
             CUSTOMERS
             </h1>
             <hr className="m-2" />
-            {!isLoading ? 
-            <div className="details-card">
-                <div className="customer-picture">
-                    <div>
-                        <img src={userDetails?.picture}/>
+            {
+                !isLoading ? 
+                <>
+                    <div className="details-card">
+                        <div className="customer-picture">
+                            <div>
+                                <img src={userDetails?.picture}/>
+                            </div>
+                            <h3>{userDetails?.username}</h3>
+                        </div>
+                        <div className="personal-info">
+                            <h3>Personal Information</h3>
+                            <p>First Name:</p>
+                            <p>Last Name:</p>
+                            <p>Email: &nbsp; {userDetails?.username}</p>
+                            <p>Phone Number: &nbsp; {userDetails?.phone}</p>
+                            <Button>Send Email</Button>
+                        </div>
                     </div>
-                    <h3>{userDetails?.username}</h3>
-                </div>
-                <div className="personal-info">
-                    <h3>Personal Information</h3>
-                    <p>First Name:</p>
-                    <p>Last Name:</p>
-                    <p>Email: &nbsp; {userDetails?.username}</p>
-                    <p>Phone Number: &nbsp; {userDetails?.phone}</p>
-                    <Button>Send Email</Button>
-                </div>
-            </div>
-            :<p>Loading...</p>
-            }
+                    <div className="location-card">
+                    <h3>Location</h3>
+                    
+                    {userDetails?.location.map((location, index)=>{
+                        return (
+                            <div className="location-details-container" key={index}>
+                                <div className="map-container">
+                                <ReactMapGL
+                                    longitude={viewports?.[index]?.longitude}
+                                    latitude={viewports?.[index]?.latitude}
+                                    zoom={13}
+                                    mapboxAccessToken= "pk.eyJ1Ijoiam1hZ3dpbGkiLCJhIjoiY2xwaGZwaHh0MDJtOTJqbzVkanpvYjRkNSJ9.fZFeViJyigw6k1ebFAbTYA"
+                                    mapStyle="mapbox://styles/mapbox/streets-v12"
+                                    interactive={true}
+                                    onDrag={(e) => {
+                                        setViewports(
+                                            viewports?.map((viewport,j) => {
+                                                return( 
+                                                    j == index 
+                                                    ? {
+                                                        longitude: e.viewState.longitude,
+                                                        latitude: e.viewState.latitude,
+                                                        zoom: viewport.zoom,    
+                                                    }
+                                                    : viewport
+                                                )
+                                            })
+                                        )  
+                                          
+                                    }}
+                                >
+                                    <Marker
+                                        latitude={location.latitude}
+                                        longitude={location.longitude}
+                                    >
+                                        <div>
+                                        <FaLocationDot
+                                            style={{
+                                            height: "40px",
+                                            width: "auto",
+                                            color: "red",
+                                            }}
+                                        />
+                                        </div>
+                                    </Marker>
+                                </ReactMapGL>
+
+                                </div>
+                                <div className="location-details">
+                                    <p>Location Name: &nbsp;{location.locName}</p>
+                                    <p>Address: &nbsp;{location.address}</p>
+                                </div>
+                            </div>                 
+                        )
+                    })}
+
+                    </div>
+                </>
+                :<p>Loading...</p>
+            }       
         </div>
     )
 }
