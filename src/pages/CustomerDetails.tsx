a0042c26196c2b18705ceb8c2967b8501dca1863
jmagwili <joshuamagwili@gmail.com>
2023-12-27 17:35:51 +0800

    integrated backend server to details card

--- src/pages/CustomerDetails.tsx
+++ src/pages/CustomerDetails.tsx
@@ -2,12 +2,41 @@ import SidebarContext from "@/SidebarContext"
 import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { Button } from "@/components/ui/button"
+import axios from "axios"
 import '../styles/CustomerDetails.css'
+
+interface UserData {
+    phone: string;
+    picture: string;
+    username: string;
+    location: [{
+        locName: string;
+        address: string;
+        latitude: number;
+        longitude: number;
+    }]
+}
 
 export default function CustomerDetails() {
     const { expanded } = useContext(SidebarContext)
     const { id } = useParams()
     const [expandedClass, setExpandedClass] = useState("")
+    const [userDetails, setUserDetails] = useState<UserData>()
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            try{
+                const userData = await axios.get(`http://localhost:4001/api/v1/customers/${id}`)
+                setUserDetails(userData.data[0])
+                console.log(userData.data[0])
+                setIsLoading(false)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchData()
+    },[])
 
     useEffect(()=>{
         setExpandedClass(!expanded ? "notExpanded" : "")
@@ -21,20 +50,25 @@ export default function CustomerDetails() {
             CUSTOMERS
             </h1>
             <hr className="m-2" />
-
+            {!isLoading ? 
             <div className="details-card">
                 <div className="customer-picture">
-                    <img src=""/>
+                    <div>
+                        <img src={userDetails?.picture}/>
+                    </div>
+                    <h3>{userDetails?.username}</h3>
                 </div>
                 <div className="personal-info">
                     <h3>Personal Information</h3>
-                    <p>First Name: {id}</p>
-                    <p>Last Name: {id}</p>
-                    <p>Email: </p>
-                    <p>Phone Number: </p>
+                    <p>First Name:</p>
+                    <p>Last Name:</p>
+                    <p>Email: &nbsp; {userDetails?.username}</p>
+                    <p>Phone Number: &nbsp; {userDetails?.phone}</p>
                     <Button>Send Email</Button>
                 </div>
             </div>
+            :<p>Loading...</p>
+            }
         </div>
     )
 }
