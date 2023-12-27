5f8b8957b5644248970ade4230c39450d3bbb71c
jmagwili <joshuamagwili@gmail.com>
2023-12-27 16:04:44 +0800

    added details card to customer details page

--- src/pages/CustomerDetails.tsx
+++ src/pages/CustomerDetails.tsx
@@ -1,21 +1,40 @@
 import SidebarContext from "@/SidebarContext"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { Button } from "@/components/ui/button"
+import '../styles/CustomerDetails.css'
 
 export default function CustomerDetails() {
     const { expanded } = useContext(SidebarContext)
     const { id } = useParams()
+    const [expandedClass, setExpandedClass] = useState("")
 
+    useEffect(()=>{
+        setExpandedClass(!expanded ? "notExpanded" : "")
+    },[expanded])
 
     return(
-        <div className="relative left-[285px]">
+        <div className={`customer-details ${expandedClass}`}>
             <h1 
             className="ml-5 mt-5 font-semibold text-gray-800 text-3xl"
             >
             CUSTOMERS
             </h1>
             <hr className="m-2" />
-            <div>This is the Customer Details Page of {id}</div>
+
+            <div className="details-card">
+                <div className="customer-picture">
+                    <img src=""/>
+                </div>
+                <div className="personal-info">
+                    <h3>Personal Information</h3>
+                    <p>First Name: {id}</p>
+                    <p>Last Name: {id}</p>
+                    <p>Email: </p>
+                    <p>Phone Number: </p>
+                    <Button>Send Email</Button>
+                </div>
+            </div>
         </div>
     )
 }
