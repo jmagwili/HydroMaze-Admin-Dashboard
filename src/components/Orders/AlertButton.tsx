c08e3b59aa0501a784051665a31977a046edfae9
jmagwili <joshuamagwili@gmail.com>
2023-12-30 11:34:28 +0800

    created AlertButton component for orders page

---
+++ src/components/Orders/AlertButton.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from "react";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+type AlertButtonProps = {
+    onContinue: () => void;
+    className?: string;
+    children: ReactNode;
+};
+
+export const AlertButton: React.FC<AlertButtonProps> = ({ children, onContinue, className }) => {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button className={className}>{children}</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
